6bc3978aeb672a75491878a5a6151c1f7cb1cd99
ashworks-l <valash1330@gmail.com>
2026-09-18 18:31:30 +0200

    ash

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ app.set("views", path.join(__dirname, "views"));
 // Static files
 app.use(express.static(path.join(__dirname, "public")));
 
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+    if (NODE_ENV === 'development') {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next();
+});
+
+// Middleware to make NODE_ENV available to all templates
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
+});
+
 // Home page
 app.get("/", (req, res) => {
     res.render("index", {
