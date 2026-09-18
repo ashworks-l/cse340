15394c99c961d8743c76681332c200265e3318c3
ashworks-l <valash1330@gmail.com>
2026-09-18 19:29:46 +0200

    ash

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 import express from 'express';
 
 import { showHomePage } from './controllers/index.js';
-import { showOrganizationsPage } from './controllers/organizations.js';
+
+import {
+    showOrganizationsPage,
+    showOrganizationDetailsPage
+} from './controllers/organizations.js';
+
 import { showProjectsPage } from './controllers/projects.js';
 import { showCategoriesPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
@@ -9,11 +14,17 @@ import { testErrorPage } from './controllers/errors.js';
 const router = express.Router();
 
 router.get('/', showHomePage);
+
 router.get('/organizations', showOrganizationsPage);
+
 router.get('/service-projects', showProjectsPage);
+
 router.get('/categories', showCategoriesPage);
 
-// error-handling routes
+// Error-handling route
 router.get('/test-error', testErrorPage);
+
+// Organization details route
+router.get('/organization/:id', showOrganizationDetailsPage);
 
 export default router;
